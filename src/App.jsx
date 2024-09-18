2c009c83745697f253ce53b0903f923842e9a422
Julio <julio.ce01@hotmail.com>
2024-09-18 12:15:02 -0600

    tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-      <div>
-        <h1>Hola, Mundo!</h1>
-      </div>
+    <h1 className="text-3xl font-bold underline bg-blue-900">
+      Hello world!
+    </h1>
     </>
   );
 }
